28034c714c6036879f332042fd8fd5e87dfcef19
Lukasz Grzelczyk <lukasz.grzelczyk@aptiv.com>
2023-12-01 16:07:34 +0100

    Initial commit

---
+++ Day1/solution.py
@@ -0,0 +1,11 @@
+
+with open('./input', mode='r') as f:
+    data = f.read()
+    ...
+
+    solution1 = 0
+    solution2 = 0
+
+
+print(f"Solution Part 1: {solution1}")
+print(f"Solution Part 2: {solution2}")
